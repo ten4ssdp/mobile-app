c9c001dfefb5e0cd53863b4d7c57dd6a72e460ec
Travailleur Lorick <loricktravailleur@gmail.com>
2020-05-14 17:20:02 +0200

    add agenda component example dummy data

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,23 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+
+import Calendar from '../../component/Calendar';
 import Center from '../../component/Center';
 
 export default function Home() {
-  const [data, setData] = useState({});
-
-  useEffect(() => {
-    async function getData() {
-      const res = await fetch('http://15.188.3.249:5000/api/');
-      const data = await res.json();
-      setData(data);
-    }
-    getData();
-  }, []);
-
   return (
-    <Center>
-      <Text>{data.message}</Text>
-      <Text>{data.description}</Text>
-    </Center>
+    <View style={{ flex: 1 }}>
+      <View style={{ height: 100 }}>
+        <Text>SamuSociale</Text>
+      </View>
+      <View style={{ flex: 1 }}>
+        <Calendar />
+      </View>
+    </View>
   );
 }
