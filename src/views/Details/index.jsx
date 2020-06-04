655983616f9f886c75a2fbd2868849cb5319696b
Travailleur Lorick <loricktravailleur@gmail.com>
2020-06-04 20:38:08 +0200

    refacto navigation details screen begin

--- src/views/Details/index.jsx
+++ src/views/Details/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { View } from 'react-native';
 
 import BackgroundImage from '../../component/BackgroundImage';
-import colors from '../../utils/colors';
 
 export default function Details({ navigation, route }) {
   const { hotel } = route.params;
