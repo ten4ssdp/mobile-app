5ce97f459f4fa2054a18ad6b179138a302768a47
Travailleur Lorick <loricktravailleur@gmail.com>
2020-06-04 19:07:48 +0200

    begin calendarCard

---
+++ src/component/CalendarCard/index.jsx
@@ -0,0 +1,13 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function CalendarCard() {
+  return (
+    <LinearGradient colors={[]}>
+      <View>
+        <Text>Hello</Text>
+      </View>
+    </LinearGradient>
+  );
+}
