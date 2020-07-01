113537156a71b2c5c84a5f1a47c8d7fb69385289
Travailleur Lorick <loricktravailleur@gmail.com>
2020-07-01 19:40:46 +0200

    render counter dinamically

--- src/component/DetailOngletVisit/index.jsx
+++ src/component/DetailOngletVisit/index.jsx
@@ -1,13 +1,23 @@
 import { Ionicons } from '@expo/vector-icons';
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import Animated from 'react-native-reanimated';
 
+import { MainStore } from '../../context/store/main';
 import colors from '../../utils/colors';
 import Bold from '../Font/Bold';
 import Light from '../Font/Light';
 
 export default function DetailOngletVisit({ y }) {
+  const [visitLength, setVisitLength] = useState(0);
+  const { state } = useContext(MainStore);
+
+  useEffect(() => {
+    if (state.currentDayVisits && state.currentDayVisits.length > visitLength) {
+      setVisitLength(state.currentDayVisits.length);
+    }
+  }, [state.currentDayVisits]);
+
   const containerHeight = y.interpolate({
     inputRange: [0, 70],
     outputRange: [50, 0],
@@ -39,9 +49,13 @@ export default function DetailOngletVisit({ y }) {
           </Light>
         </View>
       </Animated.View>
-      <View style={styles.visitProgress}>
-        <Bold style={styles.visitProgressText}>0/4</Bold>
-      </View>
+      {!!state.currentDayVisits && (
+        <View style={styles.visitProgress}>
+          <Bold
+            style={styles.visitProgressText}
+          >{`${state.currentDayVisits.length}/${visitLength}`}</Bold>
+        </View>
+      )}
     </View>
   );
 }
