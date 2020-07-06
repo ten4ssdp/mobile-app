5d71c00a3fa81138df8e7420f3d6fbf81a338ff8
Travailleur Lorick <loricktravailleur@gmail.com>
2020-07-06 09:46:54 +0200

    init map

--- src/component/VisitList/index.jsx
+++ src/component/VisitList/index.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { View } from 'react-native';
+import MapView from 'react-native-maps';
 import Animated from 'react-native-reanimated';
 
 import Bold from '../Font/Bold';
@@ -28,6 +29,14 @@ export default function VisitList({ y, navigation, visits, urgences }) {
         alignItems: 'center'
       }}
     >
+      <MapView
+        region={{
+          latitude: 48.866667,
+          longitude: 2.333333
+        }}
+        style={{ width: '100%', height: 400 }}
+      />
+
       {urgences &&
         urgences.map((urgence) => {
           return (
